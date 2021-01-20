f344a16fe5436e5bb19360e20748abdfcb746c36
Matheus <anyonebr1@gmail.com>
2021-01-20 18:19:52 -0300

    Added new saved models

--- main.py
+++ main.py
@@ -13,48 +13,49 @@ test_dataset_directory = r"C:\Users\wordp\Desktop\Fruit recognizer\fruits-360\Te
 
 image_size = (100, 100)
 batch_size = 64
-epochs = 5
+epochs = 6
 seed = 53
 
 # Refactor this
 train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-    zoom_range=0.5, horizontal_flip=True, validation_split=0.3)
+    horizontal_flip=True, vertical_flip=True, rotation_range=3, shear_range=0.1, validation_split=0.5)
 train_generator = train_datagen.flow_from_directory(
     target_size=image_size, batch_size=batch_size, directory=training_dataset_directory)
 
 valid_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-    zoom_range=0.5, horizontal_flip=True, validation_split=0.3)
+    horizontal_flip=True, vertical_flip=True, rotation_range=3, shear_range=0.1, validation_split=0.5)
 valid_generator = valid_datagen.flow_from_directory(
     target_size=image_size, batch_size=batch_size, directory=training_dataset_directory)
 
- model = tf.keras.Sequential([
+model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(128, (3, 3), activation='relu',
                            input_shape=(100, 100, 3)),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-    tf.keras.layers.Dropout(0.2),  # Drop
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
-    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.3),  # Drop
     tf.keras.layers.Dense(256, activation='relu'),
     tf.keras.layers.Dense(
         len(train_generator.class_indices), activation='softmax')
 ])
 
 early_stopping = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss', min_delta=.0020, patience=2, verbose=0,
+    monitor='val_loss', min_delta=.0045, patience=1, verbose=0,
     mode='min', baseline=None, restore_best_weights=False
 )
 
 model.compile(optimizer='adam',
               loss='categorical_crossentropy', metrics=['accuracy'])
 
-history = model.fit(train_generator, epochs=epochs, validation_data = valid_generator, verbose=1, callbacks=[early_stopping])
+history = model.fit(train_generator, epochs=epochs,
+                    validation_data=valid_generator, verbose=1, callbacks=[early_stopping])
 
-model.save('models/trained_less_augmentation_more_filters')
+model.save('models/trained_yolo')
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
